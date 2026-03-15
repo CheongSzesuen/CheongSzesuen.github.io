cc8f729d2dc7339beeba63f38b040bd97368f4c2
WaiJade <waijade@outlook.com>
2026-03-15 16:15:48 +0800

    feat(friends): 添加74的友链

--- src/content/friends.ts
+++ src/content/friends.ts
@@ -15,10 +15,19 @@ const friendsLinkSource: FriendLinkSource[] = [
   {
     name: "Zaona",
     url: "https://zaona.top/",
-    avatar: "https://zaona.top/avatar.png",
+    avatar: "avatar.png",
     description: {
       zh: "Explore The Edge Of Imagination",
       en: "Explore The Edge Of Imagination"
+    }
+  },
+  {
+    name: "hrsthrt74",
+    url: "https://hrsthrt74.github.io/",
+    avatar: "https://avatars.githubusercontent.com/u/49299205",
+    description: {
+      zh: "hrsthrt74的博客",
+      en: "hrsthrt74's Blog"
     }
   }
 ];
